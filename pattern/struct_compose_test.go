2ff13c13eb79d6f67dbc6d42d9b1ad43a0acac81
Edward <chenlulei@gmail.com>
2020-04-28 11:42:51 +0800

    add struct_compose_test

---
+++ pattern/struct_compose_test.go
@@ -0,0 +1,47 @@
+package pattern
+
+import (
+	"fmt"
+	"testing"
+)
+
+//Person 定义一个本身的人
+type Person struct {
+	Name         string
+	WalletAssets int //每个人都有钱包
+}
+
+//Tenant 租客 继承Person
+type Tenant struct {
+	Person
+	furniture string
+}
+
+//Landlord 房东，也继承Person，要收房租
+type Landlord struct {
+	Person
+	RentAccout int //房东的租金账户
+}
+
+type Mediator struct {
+	Person
+	tenant   interface{}
+	landlord interface{}
+}
+
+func TestClassCompose(t *testing.T) {
+
+	med := &Mediator{Person: Person{Name: "mediator", WalletAssets: 1001}}
+
+	landlord := &Landlord{Person: Person{Name: "landlord", WalletAssets: 2000}, RentAccout: 500}
+
+	tenant := &Tenant{Person: Person{Name: "tenant", WalletAssets: 500}, furniture: "desk"}
+
+	fmt.Println("mediator", med)
+	fmt.Println("landlord Name", landlord.Name)
+	fmt.Println("tenant furniture", tenant.furniture)
+
+	fmt.Println("mediator", med)
+	fmt.Println("landlord", landlord)
+	fmt.Println("tenant", tenant)
+}
