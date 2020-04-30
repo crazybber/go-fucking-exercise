1799841e7e1365f2de66bef36d37a6ef38b3e37d
Edward <chenlulei@gmail.com>
2020-04-30 16:25:22 +0800

    fix testing for multi type init

--- patterns/struct_compose_test.go
+++ patterns/struct_compose_test.go
@@ -31,17 +31,17 @@ type Mediator struct {
 
 func TestClassCompose(t *testing.T) {
 
-	med := &Mediator{Person: Person{Name: "mediator", WalletAssets: 1001}}
+	med := &Mediator{Person: Person{"mediator", 1001}}
 
-	landlord := &Landlord{Person: Person{Name: "landlord", WalletAssets: 2000}, RentAccout: 500}
+	landlord := &Landlord{Person: Person{"landlord", 2000}, RentAccout: 500}
 
-	tenant := &Tenant{Person: Person{Name: "tenant", WalletAssets: 500}, furniture: "desk"}
+	tenant := &Tenant{Person{"tenant", 500}, "desk"}
 
-	fmt.Println("mediator", med)
-	fmt.Println("landlord Name", landlord.Name)
-	fmt.Println("tenant furniture", tenant.furniture)
-
-	fmt.Println("mediator", med)
-	fmt.Println("landlord", landlord)
-	fmt.Println("tenant", tenant)
+	fmt.Println("mediator:", med)
+	fmt.Println("landlord Name:", landlord.Name)
+	fmt.Println("tenant furniture:", tenant.furniture)
+	fmt.Println("----------------------------")
+	fmt.Println("mediator:", med)
+	fmt.Println("landlord:", landlord)
+	fmt.Println("tenant:", tenant)
 }
