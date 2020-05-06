1916d56f3611f444f1e322c1ef95bcab6195ac2f
Edward <chenlulei@gmail.com>
2020-05-06 10:18:33 +0800

    add a test for parse int

--- basic/variables_test.go
+++ basic/variables_test.go
@@ -2,6 +2,7 @@ package basic
 
 import (
 	"fmt"
+	"strconv"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -95,4 +96,11 @@ func TestVariablesFloats(t *testing.T) {
 
 	t.Logf("f-2=%f", f-2.0)
 
+}
+
+func TestParseInt(t *testing.T) {
+	var _, err = strconv.ParseInt("127", 0, 8)
+	if err != nil {
+		fmt.Println(err)
+	}
 }
